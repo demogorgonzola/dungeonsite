28da17e57ac0aab1f8d4b3b22b9b2df4ec4f9827
ritualsfinal <solverjt@gmail.com>
2019-04-14 10:10:42 -0600

    [algo] working packing algo
    do some cleanup
    plug into resize event (on timer)
    current complexity O(n^2), find better
    
    great job!!!! :D

--- content/dnds/teem/js/packing.js
+++ content/dnds/teem/js/packing.js
@@ -28,13 +28,78 @@ function packingWidth(rows, widths) {
   }).bestWidth;
 }
 
+//O(n^2)
+function componentPacking(rows, widths) {
+  widths = widths.slice();
+  while(widths.length > rows) {
+    let smallestPair = {
+      pair: [],
+      length: widths.reduce((total,width) => total+width)
+    }
+    for( let i=1 ; i < widths.length ; i++ ) {
+      let length = widths[i-1]+widths[i];
+      if(length < smallestPair.length) {
+        smallestPair.pair = [i-1,i];
+        smallestPair.length = length;
+      }
+    }
+    console.log(widths);
+    widths.splice(smallestPair.pair[0], smallestPair.pair.length, smallestPair.length);
+  }
+  console.log(widths);
+  return widths.reduce((carry, width) => { return (width > carry) ? width : carry; }, 0);
+}
+
+function evenComponentPacking(rows, widths) {
+  let epsilon = 0.0001;
+
+  widths = widths.slice();
+
+  let evenCut = widths.reduce((total,width) => total+width)/rows;
+
+  let packWidths = [];
+  let rowLen = 0;
+  let effLen = 0;
+  for(let i=0; i<widths.length; i++) {
+    let width = widths[i];
+    if(effLen+width - evenCut > epsilon) {
+      if(rowLen != 0) {
+        packWidths.push(rowLen);
+      }
+      if(effLen+width - evenCut > epsilon) {
+        packWidths.push(width);
+        rowLen = 0;
+      } else {
+        rowLen = width;
+      }
+
+      effLen = effLen+width-evenCut;
+    } else {
+      rowLen += width;
+      effLen += width;
+    }
+  }
+  if(rowLen != 0) {
+    packWidths.push(rowLen);
+  }
+
+  return componentPacking(rows, packWidths);
+}
+
 function packContainer() {
   let children = document.currentScript.parentElement.getElementsByTagName("span");
   let childrenWidths = [...children].map(element => element.offsetWidth);
 
   let rows = 3;
-  let packWidth = packingWidth(rows,childrenWidths);
+  // let packWidth = packingWidth(rows,childrenWidths);
+  // let packWidth = componentPacking(rows,childrenWidths);
+  let packWidth = evenComponentPacking(rows,childrenWidths);
 
   console.log(childrenWidths);
   console.log(packWidth);
+
+  //temp workaround
+  packWidth += 10;
+
+  return packWidth;
 }
